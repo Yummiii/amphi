48390874a7576ffb2c27a7f3a359882d71032a9f
Yummi <yummi@zuraaa.com>
2025-06-10 17:20:15 -0300

    fix broken imports

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,23 +1,23 @@
 import { createBrowserRouter } from "react-router";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home from "./pages/home";
+import About from "./pages/about";
 import MainLayout from "./templates/main-layout";
 
 const router = createBrowserRouter([
-    {
-        path: "",
-        element: MainLayout(),
-        children: [
-            {
-                path: "/",
-                element: Home(),
-            },
-            {
-                path: "/about",
-                element: About(),
-            },
-        ],
-    },
+  {
+    path: "",
+    element: MainLayout(),
+    children: [
+      {
+        path: "/",
+        element: Home(),
+      },
+      {
+        path: "/about",
+        element: About(),
+      },
+    ],
+  },
 ]);
 
 export default router;
