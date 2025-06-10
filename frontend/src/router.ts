eabd2d1015147f5ae0c7c78c8063bbaf59c19fd1
purrleterian <jcno307@gmail.com>
2025-06-10 17:13:49 -0300

    correcao de nomes

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,23 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import MainLayout from "./templates/main-template";
+import MainLayout from "./templates/main-layout";
 
 const router = createBrowserRouter([
-  {
-    path: "",
-    element: MainLayout(),
-    children: [
-      {
-        path: "/",
-        element: Home(),
-      },
-      {
-        path: "/about",
-        element: About(),
-      },
-    ],
-  },
+    {
+        path: "",
+        element: MainLayout(),
+        children: [
+            {
+                path: "/",
+                element: Home(),
+            },
+            {
+                path: "/about",
+                element: About(),
+            },
+        ],
+    },
 ]);
 
 export default router;
