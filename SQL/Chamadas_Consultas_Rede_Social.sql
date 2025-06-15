1315594427a84ffff73e9cb04e86f10f88662ee1
AminGabriel <amingabriel123@hotmail.com>
2025-06-15 17:37:59 -0300

    SQL: functions and procedure with test data

--- SQL/Chamadas_Consultas_Rede_Social.sql
+++ SQL/Chamadas_Consultas_Rede_Social.sql
@@ -1,9 +1,9 @@
 select * from num_users;
 
-select Comment_By_User(?) as Num_Comment;
+select Comment_By_User('550e8400-e29b-41d4-a716-446655440007') as Num_Comment;
 
-select Post_By_User(?) as Num_Post;
+select Post_By_User('550e8400-e29b-41d4-a716-44665544002E') as Num_Post;
 
-call getInfoUser(?);
+call getInfoUser('julia');
 
-select Count_Reply(?) as Num_Reply;
+select Count_Reply('c0000009-0000-0000-0000-000000000009') as Num_Reply;
