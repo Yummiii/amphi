e09935aef55097481daf84326c00d7bcbae9d8e5
AminGabriel <amingabriel123@hotmail.com>
2025-06-16 19:03:57 -0300

    SQL: fixing queries and database modeling PDF

--- SQL/select_birthdays.sql
+++ SQL/select_birthdays.sql
@@ -1,4 +1,4 @@
 use rede_social;
 
 -- demonstrar os usuarios aniversariantes para adicionar na front
-SELECT *  FROM `User` WHERE MONTH(birthdate) = MONTH(NOW()) AND DAY(birthdate) = DAY(NOW());
+SELECT id, username  FROM `User` WHERE MONTH(birthdate) = MONTH(NOW()) AND DAY(birthdate) = DAY(NOW());
