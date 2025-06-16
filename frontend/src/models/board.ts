5309a14313f03cd9cd85f0442cd1c8276d6615b2
Yummi <yummi@zuraaa.com>
2025-06-15 21:55:52 -0300

    fix board image

--- frontend/src/models/board.ts
+++ frontend/src/models/board.ts
@@ -2,24 +2,24 @@ import type { Post } from "./post";
 import type { User } from "./user";
 
 export const BoardMemberRoles = {
-    Admin: 0,
-    Member: 1,
+  Admin: 0,
+  Member: 1,
 } as const;
 
 export type BoardMemberRoles =
-    (typeof BoardMemberRoles)[keyof typeof BoardMemberRoles];
+  (typeof BoardMemberRoles)[keyof typeof BoardMemberRoles];
 
 export interface Board {
-    id: number;
-    name: string;
-    slug: string;
-    description: string;
-    posts: Post[];
-    members: BoardMember[];
-    image: string;
+  id: number;
+  name: string;
+  slug: string;
+  description: string;
+  posts: Post[];
+  members: BoardMember[];
+  image: string | null;
 }
 
 export interface BoardMember {
-    role: BoardMemberRoles;
-    user: User;
+  role: BoardMemberRoles;
+  user: User;
 }
